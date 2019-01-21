8a77a5036aeb5036c2dccd64582451bc1b14234e
telamonian <mklein@jhu.edu>
2019-01-20 23:37:03 -0500

    starting the Darcula theme

---
+++ README.md
@@ -0,0 +1,2 @@
+@telamonian/theme-darcula
+theme for Jupyterlab. Cozy like Intellij on a Sunday morning
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+
+import {
+  JupyterLab, JupyterLabPlugin
+} from '@jupyterlab/application';
+
+import {
+  IThemeManager
+} from '@jupyterlab/apputils';
+
+
+/**
+ * A plugin for the Telamonian Darcula
+ */
+const plugin: JupyterLabPlugin<void> = {
+  id: '@telamonian/theme-darcula:plugin',
+  requires: [IThemeManager],
+  activate: function(app: JupyterLab, manager: IThemeManager) {
+    manager.register({
+      name: 'Telamonian Darcula',
+      isLight: false,
+      load: function() {
+        return manager.loadCSS('telamonian-theme-darcula/index.css');
+      },
+      unload: function() {
+        return Promise.resolve(void 0);
+      }
+    });
+  },
+  autoStart: true
+};
+
+
+export default plugin;
