e05a348fc908c24375a66171925e5e26731a584a
telamonian <mklein@jhu.edu>
2019-02-19 01:16:31 -0500

    backported a lot of improvements from telamonian/theme-darcula:jlab-1.0

---
+++ .ci_funcs.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+set -ex
+set -o pipefail
+
+ci_install() {
+    # Install JupyterLab and enable the server extension
+    pip install -q --upgrade pip
+    pip --version
+    pip install cookiecutter jupyterlab
+    jupyter serverextension enable --py jupyterlab
+}
+
+ci_script() {
+    # set a custom user-settings directory
+    export JUPYTERLAB_SETTINGS_DIR="./.jupyter/lab/user-settings"
+    THEME_SETTINGS="${JUPYTERLAB_SETTINGS_DIR}/@jupyterlab/apputils-extension/themes.jupyterlab-settings"
+
+    # Create a theme extension using the cookiecutter default inputs
+    # cookiecutter . --no-input
+
+    # install theme
+    jupyter labextension install .
+
+    # enable the theme in user-settings
+    mkdir -p $(dirname $THEME_SETTINGS)
+    printf "%s\n" "{" "    \"theme\": \"theme-darcula\"" "}" > $THEME_SETTINGS
+
+    # print out a confirmation that the extension is installed
+    jupyter labextension list
+
+    # run a test of the main JupyterLab app with the theme enabled
+    python -m jupyterlab.browser_check
+}
--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-# @telamonian-theme-darcula
+# @telamonian/theme-darcula
+
+[![Build Status](https://travis-ci.org/telamonian/theme-darcula.svg?branch=master)](https://travis-ci.org/telamonian/theme-darcula)
 
 Darcula theme for Jupyterlab. Modelled after the classic Intellij theme.
+
+![darcula_preview](darcula_preview.png)
+
+## Prerequisites
+
+* JupyterLab
+
+## Installation
+
+```bash
+jupyter labextension install @telamonian/theme-darcula
+```
+
+## Development
+
+For a development install (requires npm version 4 or later), do the following in the repository directory:
+
+```bash
+npm install
+jupyter labextension link .
+```
+
+To rebuild the package and the JupyterLab app:
+
+```bash
+npm run build
+jupyter lab build
+```
