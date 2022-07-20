e9e49257bcb78171f41ada82b0184246c0f59f8d
telamonian <telamonian@users.noreply.github.com>
2022-07-20 07:51:16 -0400

    update for jlab 4.x compatibility

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup_args = dict(
     cmdclass=cmdclass,
     packages=setuptools.find_packages(),
     install_requires=[
-        "jupyterlab>=3.0.0rc13,==3.*",
+        "jupyterlab>=3.0.0",
     ],
     zip_safe=False,
     include_package_data=True,
